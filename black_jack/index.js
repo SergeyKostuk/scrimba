e111e8bae63cb4dd7fe56c7b5cb8723e3a0b8fe5
Sergey Kostyuk <s.kostyuk@invento.by>
2023-02-21 20:13:21 +0300

    Continue developing scrimba blackjack game

--- black_jack/index.js
+++ black_jack/index.js
@@ -3,25 +3,78 @@
 
 // 2. Create a variable, sum, and set it to the sum of the two cards
 
-let firstCard = 3;
-let secondCard = 6;
-let sum = firstCard + secondCard;
+
+let cards =  []
+let sum = 0;
 let hasBlaclJack = false;
-let isAlive = true;
+let isAlive = false;
 let message = "";
 let messageEl = document.getElementById("message-el")
 let sumEl = document.querySelector("#sum-el")
+let cardsEl = document.querySelector("#cards-el")
 
-function startGame() {
+let player = {
+   name : "MR. X",
+   chips : 145
+}
+
+
+
+let playerEl = document.getElementById("player-el")
+playerEl.textContent = player.name + ": $" + player.chips
+
+function startGame(){
+  isAlive = true
+  let firstCard = getRandomCard()
+  let secondCard = getRandomCard()
+  cards = [firstCard, secondCard]
+  sum = firstCard + secondCard
+  hasBlaclJack = false
+  renderGame()
+}
+
+
+function renderGame() {
   if (sum < 21) {
-    message = "Do you want to draw a new card? 🙂";
+    message = "Do you want to draw a new card?";
   } else if (sum === 21) {
-    message = "Wohoo! You've got Blackjack! 🥳";
+    message = "You've got Blackjack!";
     hasBlaclJack = true;
   } else {
-    message = "You're out of the game! 😭";
+    message = "You're out of the game!";
     isAlive = false;
   }
   messageEl.textContent  = message
+
   sumEl.textContent = 'Sum: ' + sum
+
+  cardsEl.textContent = "Cards: "
+  for (let count = 0; count < cards.length; count++){
+    cardsEl.textContent  += cards[count] + ' '
+  }
+  
+}
+
+function newCard() {
+  if (isAlive && !hasBlaclJack){
+  let card = getRandomCard()
+  cards.push(card)
+  sum += card
+  renderGame()}
+  
+}
+
+function getRandomCard() {
+  let randomCard = Math.floor( Math.random() * 13) + 1
+  if (randomCard > 10){
+    return 10 
+  }
+  else if (randomCard === 1){
+    return 11
+  }
+  else {return randomCard}
+   
+  
+  return Math.floor( Math.random() * 13) + 1
+  
 }
