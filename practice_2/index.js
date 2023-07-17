273be3e445f8dfd5bcf90bde5e4f56e353474fcc
Sergey Kostyuk <s.kostyuk@invento.by>
2023-07-17 23:00:16 +0300

    practice + build extension

---
+++ practice_2/index.js
@@ -0,0 +1,11 @@
+let person = {
+    name: "Sergey",
+    age: 23,
+    country: "RB"
+}
+
+function logData() {
+    console.log(person.name + " is " + person.age + " years old and lives in " + person.country)
+}
+
+logData()
